72ece5abe70e967edcef60a9cd6df176126413fd
ben-smith23 <112433581+ben-smith23@users.noreply.github.com>
2022-12-09 10:52:29 -0800

    commit

--- project.py
+++ project.py
@@ -11,7 +11,16 @@ After doing do, this file should "just work".
 '''
 
 from flask import Flask, render_template, send_from_directory, request, make_response
+from flask_babel import Babel, gettext, ngettext
+
+
 app = Flask(__name__)
+app.config.['BABEL_DEFAULT_LOCALE'] = 'en'
+babel = Babel(app)
+
+def get_locale():
+    return 'en'
+    request.accept_languages.best({})
 
 import sqlite3
 
@@ -41,49 +50,103 @@ def root():
             'username': row_users[0],
             'age': row_users[1]
         })
+
+    # check if logged in correctly
+    username = request.cookies.get('username')
+    password = request.cookies.get('password')
+    good_credentials = are_credentials_good(username, password)
+    print('good_credentials=', good_credentials)
     return render_template('root.html', messages=messages, logged_in = False)
+
+def print_debug_info():
+    # GET method
+    print('request.args.get("username")=', request.args.get("username"))
+    print('request.args.get("password")=', request.args.get("password"))
+
+    # POST method
+    print('request.form.get("username")=', request.form.get("username"))
+    print('request.form.get("password")=', request.form.get("password"))
+
+    # cookies
+    print('request.cookies.get("username")=', request.cookies.get("username"))
+    print('request.cookies.get("password")=', request.cookies.get("password"))
 
 def are_credentials_good(username, password):
     # FIXME:
-    # look in db and confirm password is correct
-    if username == 'haxor' and password == 'truth':
+    # look inside the databasse and check if the password is correct for the user
+    if username == 'haxor' and password == '1337':
         return True
+    else:
+        return False
 
 @app.route('/login', methods=['GET', 'POST'])     
 def login():
-    username = request.args.get('username')
-    password = request.args.get('password')
+    print_debug_info()
+    # requests (plural) library for downloading;
+    # now we need request singular 
+    username = request.form.get('username')
+    password = request.form.get('password')
+    print('username=', username)
+    print('password=', password)
 
     good_credentials = are_credentials_good(username, password)
+    print('good_credentials=', good_credentials)
 
+    # the first time we've visited, no form submission
     if username is None:
         return render_template('login.html', bad_credentials=False)
-        
-    else:   
+
+    # they submitted a form; we're on the POST method
+    else:
         if not good_credentials:
             return render_template('login.html', bad_credentials=True)
         else:
+            # if we get here, then we're logged in
+            #return 'login successful'
+
+            # create a cookie that contains the username/password info
+
             template = render_template(
-                'login.html',
-                bad_credentials = False,
-                logged_in = True
+                'login.html', 
+                bad_credentials=False,
+                logged_in=True)
+            #return template
             response = make_response(template)
             response.set_cookie('username', username)
-            response.set_cookie('username', username)
+            response.set_cookie('password', password)
             return response
-            )
 
 @app.route('/logout')     
 def logout():
     return render_template('logout.html')
 
-@app.route('/create_user')     
+@app.route('/create_user', methods=['GET', 'POST'])     
 def create_user():
+    try:
+        con = sqlite3.connect(args.db_file)
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirm_password = request.form.get('password')
+
+    good_credentials = are_credentials_good(username, password)
+    print('good_credentials=', good_credentials)
+
+    # the first time we've visited, no form submission
+    if username is None:
+        return render_template('login.html', bad_credentials=False)
     return render_template('create_user.html')
 
 @app.route('/create_message')     
 def create_message():
     return render_template('create_message.html')
+
+@app.route('/delete_message')     
+def delete_message():
+    return render_template('delete_message.html')
+
+@app.route('/edit_message')     
+def edit_message():
+    return render_template('edit_message.html')
 
 @app.route('/static/<path>')     
 def static_directory(path):
